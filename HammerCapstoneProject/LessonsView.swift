6ddb1caa61c4866e97eefafb9fd8b6fe5d86a0ac
Valerie Hammer <vkellogg@wisc.edu>
2025-05-05 15:41:14 -0500

    Created UI Views

---
+++ HammerCapstoneProject/LessonsView.swift
@@ -0,0 +1,18 @@
+//
+//  LessonsView.swift
+//  HammerCapstoneProject
+//
+//  Created by Valerie Hammer on 5/5/25.
+//
+
+import SwiftUI
+
+struct LessonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LessonsView()
+}
