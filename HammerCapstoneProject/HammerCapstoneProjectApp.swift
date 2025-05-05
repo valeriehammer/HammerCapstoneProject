ae2b8ad91d8affe3bfc75a0cc87e497ee4f7e1e8
Valerie Hammer <vkellogg@wisc.edu>
2025-05-05 12:34:06 -0500

    Initial Commit

---
+++ HammerCapstoneProject/HammerCapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HammerCapstoneProjectApp.swift
+//  HammerCapstoneProject
+//
+//  Created by Valerie Hammer on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HammerCapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
